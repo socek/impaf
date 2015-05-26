5cb815045b21d0e4c25281d0c121852830a5941d
Dominik "Socek" Dlugajczyk <msocek@gmail.com>
2015-05-26 22:20:32 +0200

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,19 @@
+# -*- encoding: utf-8 -*-
+from setuptools import setup, find_packages
+
+install_requires = [
+    'pyramid',
+]
+
+
+if __name__ == '__main__':
+    setup(
+        name='impaf',
+        version='0.1',
+        description='Inherit Me Please Anti Framework',
+        url='https://github.com/socek/impaf',
+        license='Apache License 2.0',
+        packages=find_packages('src'),
+        package_dir={'': 'src'},
+        install_requires=install_requires,
+    )
