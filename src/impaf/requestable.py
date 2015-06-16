0a19796a339a8b05bacbcd0ee3736b6de07d38d0
Dominik "Socek" Dlugajczyk <msocek@gmail.com>
2015-06-16 07:43:47 +0200

    Add some params to Requestable.feed_request.

--- src/impaf/requestable.py
+++ src/impaf/requestable.py
@@ -2,3 +2,10 @@ class Requestable(object):
 
     def feed_request(self, request):
         self.request = request
+        self.registry = request.registry
+        self.POST = request.POST
+        self.GET = request.GET
+        self.matchdict = request.matchdict
+        self.settings = self.registry['settings']
+        self.paths = self.registry['paths']
+        self.route_path = request.route_path
